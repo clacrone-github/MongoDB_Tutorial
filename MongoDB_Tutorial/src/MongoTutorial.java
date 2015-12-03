7e643739161d7fc145cc328017db1ebbce979bd3
Craig LaCrone <clacrone@tableausoftware.com>
2015-12-03 10:03:13 -0800

    added comments for clarity

--- MongoDB_Tutorial/src/MongoTutorial.java
+++ MongoDB_Tutorial/src/MongoTutorial.java
@@ -27,27 +27,42 @@ public class MongoTutorial {
 		MongoUtil util = new MongoUtil("localhost");
 		
 		MongoClient mongoClient = util.getMongoClient();
+		
+		// Get the database
 		DB myDb = mongoClient.getDB("myDb");
 		
+		// Get the collection
 		DBCollection coll = myDb.getCollection("testCollection");
+		
+		// Drop existing table (temporary)
+		coll.drop();
+		
+		//JOURNALED: Exceptions are raised for network issues, and server errors; 
+		//the write operation waits for the server to group commit to the journal file on disk.
 		mongoClient.setWriteConcern(WriteConcern.JOURNALED);
 		
+		//Create object
 		BasicDBObject doc = new BasicDBObject("name", "MongoDB")
         .append("type", "database")
         .append("count", 1)
         .append("info", new BasicDBObject("x", 203).append("y", 102));
 		
+		//Insert object
 		coll.insert(doc);
 		
+		// Get a document
 		DBObject myDoc = coll.findOne();
 		System.out.println(myDoc);
 		
+		// Insert 100 objects
 		for (int i=0; i < 100; i++) {
 		    coll.insert(new BasicDBObject("i", i));
 		}
 		
+		// Get the count of objects
 		System.out.println(coll.getCount());
 
+		// Get all documents, and iterate through them
 		DBCursor cursor = coll.find();
 		try {
 		   while(cursor.hasNext()) {
@@ -57,10 +72,9 @@ public class MongoTutorial {
 		   cursor.close();
 		}
 
+		// Find object #71 and print it
 		BasicDBObject query = new BasicDBObject("i", 71);
-
 		cursor = coll.find(query);
-
 		try {
 		   while(cursor.hasNext()) {
 		       System.out.println(cursor.next());
@@ -69,11 +83,10 @@ public class MongoTutorial {
 		   cursor.close();
 		}
 		
+		// find all j != 3, and k > 10 (there are none)
 		query = new BasicDBObject("j", new BasicDBObject("$ne", 3))
         .append("k", new BasicDBObject("$gt", 10));
-
 		cursor = coll.find(query);
-		
 		try {
 		    while(cursor.hasNext()) {
 		        System.out.println(cursor.next());
@@ -82,10 +95,9 @@ public class MongoTutorial {
 		    cursor.close();
 		}
 		
-		
+		// find all i, > 20 and <= 30 and print
 		query = new BasicDBObject("i", new BasicDBObject("$gt", 20).append("$lte", 30));
 		cursor = coll.find(query);
-
 		try {
 		    while (cursor.hasNext()) {
 		        System.out.println(cursor.next());
@@ -96,6 +108,7 @@ public class MongoTutorial {
 
 		// 1. Ordered bulk operation
 		BulkWriteOperation builder = coll.initializeOrderedBulkOperation();
+		
 		builder.insert(new BasicDBObject("_id", 1));
 		builder.insert(new BasicDBObject("_id", 2));
 		builder.insert(new BasicDBObject("_id", 3));
@@ -110,7 +123,7 @@ public class MongoTutorial {
 		builder = coll.initializeUnorderedBulkOperation();
 		builder.find(new BasicDBObject("_id", 1)).removeOne();
 		builder.find(new BasicDBObject("_id", 2)).removeOne();
-
+		
 		result = builder.execute();
 	}
 
